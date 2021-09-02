40ba5abfa76c8b9775df254be976a9b0a3a14f54
ArturKardas <kardasartur1@gmail.com>
2021-09-03 01:00:05 +0200

    usuniecie bulmy

--- my-site/src/app/app-routing.module.ts
+++ my-site/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { AboutMeComponent } from './about-me/about-me.component';
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AboutMeComponent},
+  { path: 'about-me', component: AboutMeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-site/src/app/app.module.ts
+++ my-site/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainSiteContentComponent } from './main-site-content/main-site-content.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainSiteContentComponent
+    MainSiteContentComponent,
+    AboutMeComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
