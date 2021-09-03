bd4ed0274bbd3f31d265e01ecba479a7dc17c4d0
ArturKardas <kardasartur1@gmail.com>
2021-09-03 16:53:58 +0200

    nav-bar po zmiane css na scss

--- my-site/src/app/main-site-content/main-site-content.component.ts
+++ my-site/src/app/main-site-content/main-site-content.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-site-content',
   templateUrl: './main-site-content.component.html',
-  styleUrls: ['./main-site-content.component.css']
+  styleUrls: ['./main-site-content.component.scss']
 })
 export class MainSiteContentComponent implements OnInit {
 
